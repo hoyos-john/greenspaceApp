45d6c61eed5b219e63b8e90ea9b931da2cee2446
hoyos-john <79256323+hoyos-john@users.noreply.github.com>
2023-04-26 01:20:12 +0100

    Conservation Events Page started
    Google Maps dependency added, prior to starting the implementation of the map.

--- app/src/main/java/com/example/greenspace_project/MainActivity.java
+++ app/src/main/java/com/example/greenspace_project/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.greenspace_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Object for the button
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialise the button
+        button = findViewById(R.id.loginButton);
+
+        //Set onClick Listener
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent to call new activity ,the below code does not include this
+                Intent intent = new Intent(getApplicationContext(), ConservationEvents.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
